79afaad18269439e681bbb5fe280a099e33012ce
JohnPeng <xsyn74@gmail.com>
2015-10-05 20:40:01 +0800

    delete lines

--- src/LayerTest.js
+++ src/LayerTest.js
@@ -111,8 +111,6 @@ var LayerTest2 = LayerTest.extend({
         layer1.y = s.height / 2;
         layer1.ignoreAnchorPointForPosition(false);
         this.addChild(layer1, 1, LAYERTEST2_LAYER1_TAG);
-
-
     }
 });
 
