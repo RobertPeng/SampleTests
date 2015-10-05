0ff8e1df36b5f3169121fbf9dfe5b18eb01dfdfc
JohnPeng <xsyn74@gmail.com>
2015-10-05 21:59:28 +0800

    event test

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ cc.game.onStart = function(){
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
         //cc.director.runScene(new NodeTestScene());
-        new LayerTestScene().runThisTest();
+        new EventDispatcherTestScene().runThisTest();
     }, this);
 };
 cc.game.run();
